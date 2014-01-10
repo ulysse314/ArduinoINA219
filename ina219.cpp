7c3eb529fd3536d68d4d582c3142049e956d8a93
Jukka-Pekka Sarjanen <jukka-Pekka.sarjanen@kone.com>
2014-01-10 22:54:51 +0200

    Fix calibrate function and several others C++ and documenting relative changes, anyway intermediate commit

--- ina219.cpp
+++ ina219.cpp
@@ -36,133 +36,145 @@
 
 #include "INA219.H"
 #include <util/delay.h>
+namespace{
+// config. register bit labels
+const uint8_t RST =	15;
+const uint8_t BRNG = 13;
+const uint8_t PG1 = 12;
+const uint8_t PG0 = 11;
+const uint8_t BADC4 = 10;
+const uint8_t BADC3	= 9;
+const uint8_t BADC2	= 8;
+const uint8_t BADC1	= 7;
+const uint8_t SADC4	= 6;
+const uint8_t SADC3	= 5;
+const uint8_t SADC2	= 4;
+const uint8_t SADC1	= 3;
+const uint8_t MODE3	= 2;
+const uint8_t MODE2	= 1;
+const uint8_t MODE1	= 0;
+};
 
-INA219::INA219() {
+INA219::INA219(t_i2caddr addr): i2c_address(addr) {
 }
 
-
-void INA219::begin(uint8_t addr)
-{
-  Wire.begin();
-  i2c_address = addr;
-  gain = D_GAIN;
+void INA219::begin() {
+    Wire.begin();
 }
 
-
-// calibration of equations and device
-// shunt_val 		= value of shunt in Ohms
-// v_shunt_max 		= maximum value of voltage across shunt
-// v_bus_max 		= maximum voltage of bus
-// i_max_expected 	= maximum current draw of bus + shunt
-// default values are for a 0.25 Ohm shunt on a 5V bus with max current of 1A
-void INA219::calibrate(float shunt_val, float v_shunt_max, float v_bus_max, float i_max_expected)
-{
-  uint16_t cal;
-  float i_max_possible, min_lsb, max_lsb, swap;
-
-  r_shunt = shunt_val;
-
-  i_max_possible = v_shunt_max / r_shunt;
-  min_lsb = i_max_expected / 32767;
-  max_lsb = i_max_expected / 4096;
-
-  current_lsb = (uint16_t)(min_lsb * 100000000) + 1;
-  current_lsb /= 100000000;
-  swap = (0.04096)/(current_lsb*r_shunt);
-  cal = (uint16_t)swap;
-  power_lsb = current_lsb * 20;
+void INA219::calibrate(float shunt_val, float v_shunt_max, float v_bus_max, float i_max_expected) {
+    uint16_t cal,digits;
+    float i_max_possible, min_lsb, max_lsb, swap;
 
 #if (INA219_DEBUG == 1)
-  Serial.print("v_bus_max:	"); Serial.println(v_bus_max, 8);
-  Serial.print("v_shunt_max:	"); Serial.println(v_shunt_max, 8);
-  Serial.print("i_max_possible:	"); Serial.println(i_max_possible, 8);
-  Serial.print("i_max_expected: "); Serial.println(i_max_expected, 8);
-  Serial.print("min_lsb:	"); Serial.println(min_lsb, 12);
-  Serial.print("max_lsb:	"); Serial.println(max_lsb, 12);
-  Serial.print("current_lsb:	"); Serial.println(current_lsb, 12);
-  Serial.print("power_lsb:	"); Serial.println(power_lsb, 8);
-  Serial.println("  ");
-  Serial.print("cal:		"); Serial.println(cal);
-  Serial.print("r_shunt:	"); Serial.println(r_shunt);
+     float max_current,max_before_overflow,max_shunt_v,max_shunt_v_before_overflow,max_power;
 #endif
+    r_shunt = shunt_val;
 
-  write16(CAL_R, cal);
+    i_max_possible = v_shunt_max / r_shunt;
+    min_lsb = i_max_expected / 32767;
+    max_lsb = i_max_expected / 4096;
+
+    current_lsb = min_lsb;
+    digits=0;
+
+    /* From datasheet: This value was selected to be a round number near the Minimum_LSB. This selection allows for good resolution with a rounded LSB.
+	 * eg. 0.000610 -> 0.000700
+	*/
+    while( current_lsb > 0.0 ){//If zero there is something weird...
+        if( (uint16_t)current_lsb / 1){
+        	current_lsb = (uint16_t) current_lsb + 1;
+        	current_lsb /= pow(10,digits);
+        	break;
+        }
+        else{
+        	digits++;
+            current_lsb *= 10.0;
+        }
+    };
+
+    swap = (0.04096)/(current_lsb*r_shunt);
+    cal = (uint16_t)swap;
+    power_lsb = current_lsb * 20;
+
+#if (INA219_DEBUG == 1)
+      max_current = current_lsb*32767;
+      max_before_overflow =  max_current > i_max_possible?i_max_possible:max_current;
+
+      max_shunt_v = max_before_overflow*r_shunt;
+      max_shunt_v_before_overflow = max_shunt_v > v_shunt_max?v_shunt_max:max_shunt_v;
+
+      max_power = v_bus_max * max_before_overflow;
+      Serial.print("v_bus_max:     "); Serial.println(v_bus_max, 8);
+      Serial.print("v_shunt_max:   "); Serial.println(v_shunt_max, 8);
+      Serial.print("i_max_possible:        "); Serial.println(i_max_possible, 8);
+      Serial.print("i_max_expected: "); Serial.println(i_max_expected, 8);
+      Serial.print("min_lsb:       "); Serial.println(min_lsb, 12);
+      Serial.print("max_lsb:       "); Serial.println(max_lsb, 12);
+      Serial.print("current_lsb:   "); Serial.println(current_lsb, 12);
+      Serial.print("power_lsb:     "); Serial.println(power_lsb, 8);
+      Serial.println("  ");
+      Serial.print("cal:           "); Serial.println(cal);
+      Serial.print("r_shunt:       "); Serial.println(r_shunt);
+      Serial.print("max_before_overflow:       "); Serial.println(max_before_overflow,8);
+      Serial.print("max_shunt_v_before_overflow:       "); Serial.println(max_shunt_v_before_overflow,8);
+      Serial.print("max_power:       "); Serial.println(max_power,8);
+
+#endif
+      write16(CAL_R, cal);
 
 }
 
-
-// config values (range, gain, bus adc, shunt adc, mode) can be derived from pp26-27 in the datasheet
-// defaults are:
-// range = 1 (0-32V bus voltage range)
-// gain = 3 (1/8 gain - 320mV range)
-// bus adc = 3 (12-bit, single sample, 532uS conversion time)
-// shunt adc = 3 (12-bit, single sample, 532uS conversion time)
-// mode = 7 (continuous conversion)
-void INA219::configure(uint8_t range, uint8_t gain, uint8_t bus_adc, uint8_t shunt_adc, uint8_t mode)
-{
+void INA219::configure(  t_range range,  t_gain gain,  t_adc  bus_adc,  t_adc shunt_adc,  t_mode mode) {
   config = 0;
 
   config |= (range << BRNG | gain << PG0 | bus_adc << BADC1 | shunt_adc << SADC1 | mode);
 
-  write16(CONFIG_R, config);		
+  write16(CONFIG_R, config);
 }
 
-// resets the INA219
-void INA219::reset()
-{
+#define INA_RESET        0xFFFF    // send to CONFIG_R to reset unit
+void INA219::reset(){
   write16(CONFIG_R, INA_RESET);
   _delay_ms(5);
 }
 
-// returns the raw binary value of the shunt voltage
-int16_t INA219::shuntVoltageRaw()
-{
+int16_t INA219::shuntVoltageRaw() const {
   return read16(V_SHUNT_R);
 }
 
-// returns the shunt voltage in volts.
-float INA219::shuntVoltage()
-{
+float INA219::shuntVoltage() const {
   float temp;
   temp = read16(V_SHUNT_R);
   return (temp / 100000);
 }
 
-// returns raw bus voltage binary value
-int16_t INA219::busVoltageRaw()
-{
+int16_t INA219::busVoltageRaw() const {
   return read16(V_BUS_R);
 }
 
-// returns the bus voltage in volts
-float INA219::busVoltage()
-{
+
+float INA219::busVoltage() const {
   int16_t temp;
   temp = read16(V_BUS_R);
   temp >>= 3;
   return (temp * 0.004);
 }
 
-// returns the shunt current in amps
-float INA219::shuntCurrent()
-{
+float INA219::shuntCurrent() const {
   return (read16(I_SHUNT_R) * current_lsb);
 }
 
-// returns the bus power in watts
-float INA219::busPower()
-{
+float INA219::busPower() const {
   return (read16(P_BUS_R) * power_lsb);
 }
 
 
 /**********************************************************************
-* 			INTERNAL I2C FUNCTIONS			      *
+*             INTERNAL I2C FUNCTIONS                  *
 **********************************************************************/
 
-// writes a 16-bit word (d) to register pointer (a)
-// when selecting a register pointer to read from, (d) = 0
-void INA219::write16(uint8_t a, uint16_t d) {
+void INA219::write16(t_reg a, uint16_t d) const {
   uint8_t temp;
   temp = (uint8_t)d;
   d >>= 8;
@@ -182,14 +194,13 @@ void INA219::write16(uint8_t a, uint16_t d) {
   delay(1);
 }
 
-
-int16_t INA219::read16(uint8_t a) {
+int16_t INA219::read16(t_reg a) const {
   uint16_t ret;
 
   // move the pointer to reg. of interest, null argument
   write16(a, 0);
   
-  Wire.requestFrom((int)i2c_address, 2);	// request 2 data bytes
+  Wire.requestFrom((int)i2c_address, 2);    // request 2 data bytes
 
   #if ARDUINO >= 100
     ret = Wire.read(); // rx hi byte
@@ -205,4 +216,3 @@ int16_t INA219::read16(uint8_t a) {
 
   return ret;
 }
-
