e3b7c7196625381a9679022f2395005cf863d471
moussaillon <moussaillon>
2019-02-14 20:10:24 +0100

    Update ina219_test with the new API

--- examples/ina219_test/ina219_test.ino
+++ examples/ina219_test/ina219_test.ino
@@ -16,8 +16,11 @@ INA219 monitor;
 
 void setup()
 {
-  Serial.begin(9600);
-  monitor.begin();
+  Serial.begin(115200);
+  bool result = monitor.begin();
+  return;
+  Serial.print("Monitor begin: ");
+  Serial.println(result ? "true" : "false");
   // begin calls:
   // configure() with default values RANGE_32V, GAIN_8_320MV, ADC_12BIT, ADC_12BIT, CONT_SH_BUS
   // calibrate() with default values D_SHUNT=0.1, D_V_BUS_MAX=32, D_V_SHUNT_MAX=0.2, D_I_MAX_EXPECTED=2
@@ -26,37 +29,70 @@ void setup()
 
 void loop()
 {
+  Serial.println("ttest");
+  delay(1000);
+  return;
 
   Serial.println("******************");
   
   Serial.print("raw shunt voltage: ");
-  Serial.println(monitor.shuntVoltageRaw());
+  int16_t shuntVoltageRaw;
+  if (monitor.shuntVoltageRaw(&shuntVoltageRaw)) {
+    Serial.println(shuntVoltageRaw);
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.print("raw bus voltage:   ");
-  Serial.println(monitor.busVoltageRaw());
+  int16_t busVoltageRaw;
+  if (monitor.busVoltageRaw(&busVoltageRaw)) {
+    Serial.println(busVoltageRaw);
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.println("--");
   
   Serial.print("shunt voltage: ");
-  Serial.print(monitor.shuntVoltage() * 1000, 4);
-  Serial.println(" mV");
+  float shuntVoltage;
+  if (monitor.shuntVoltage(&shuntVoltage)) {
+    Serial.print(shuntVoltage * 1000, 4);
+    Serial.println(" mV");
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.print("shunt current: ");
-  Serial.print(monitor.shuntCurrent() * 1000, 4);
-  Serial.println(" mA");
+  float shuntCurrent;
+  if (monitor.shuntCurrent(&shuntCurrent)) {
+    Serial.print(shuntCurrent * 1000, 4);
+    Serial.println(" mA");
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.print("bus voltage:   ");
-  Serial.print(monitor.busVoltage(), 4);
-  Serial.println(" V");
+  float busVoltage;
+  if (monitor.busVoltage(&busVoltage)) {
+    Serial.print(busVoltage, 4);
+    Serial.println(" V");
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.print("bus power:     ");
-  Serial.print(monitor.busPower() * 1000, 4);
-  Serial.println(" mW");
+  float busPower;
+  if (monitor.busPower(&busPower)) {
+    Serial.print(busPower * 1000, 4);
+    Serial.println(" mW");
+  } else {
+    Serial.println("failed");
+  }
   
   Serial.println(" ");
   Serial.println(" ");
   
-  delay(10000);
+  delay(1000);
 
 }
 
