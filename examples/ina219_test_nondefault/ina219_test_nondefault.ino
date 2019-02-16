c0cbbeab758e2a5c6bd04f44b1533c4bd3a560dd
moussaillon <moussaillon>
2019-02-16 18:38:16 +0100

    Updating ina219_test_nondefault.ino to the new API

--- examples/ina219_test_nondefault/ina219_test_nondefault.ino
+++ examples/ina219_test_nondefault/ina219_test_nondefault.ino
@@ -9,57 +9,88 @@
 #include <Wire.h>
 #include <INA219.h>
 
-#define SHUNT_MAX_V 0.04  /* Rated max for our shunt is 75mv for 50 A current: 
-                             we will mesure only up to 20A so max is about 75mV*20/50 lets put some more*/
-#define BUS_MAX_V   16.0  /* with 12v lead acid battery this should be enough*/
-#define MAX_CURRENT 20    /* In our case this is enaugh even shunt is capable to 50 A*/
-#define SHUNT_R   0.015   /* Shunt resistor in ohm */
+#define MAX_CURRENT 20    /* In our case this is enough even shunt is capable to 50 A*/
+#define SHUNT_R   0.00125   /* Shunt resistor in ohm */
 
-INA219 monitor;
+INA219 monitor(&Wire, 0x41);
+bool beginResult;
 
 void setup()
 {
-  Serial.begin(57600);
-  monitor.begin();
+  Serial.begin(115200);
+  Wire.begin();
+  beginResult = monitor.begin();
   // setting up our configuration
   // default values are RANGE_32V, GAIN_8_320MV, ADC_12BIT, ADC_12BIT, CONT_SH_BUS
   monitor.configure(INA219::RANGE_16V, INA219::GAIN_2_80MV, INA219::ADC_64SAMP, INA219::ADC_64SAMP, INA219::CONT_SH_BUS);
   
   // calibrate with our values
-  monitor.calibrate(SHUNT_R, SHUNT_MAX_V, BUS_MAX_V, MAX_CURRENT);
+  monitor.calibrate(SHUNT_R, MAX_CURRENT);
 }
 
 void loop()
 {
   Serial.println("******************");
-  
+
+  Serial.print("Monitor begin: ");
+  Serial.println(beginResult ? "true" : "false");
   Serial.print("raw shunt voltage: ");
-  Serial.println(monitor.shuntVoltageRaw());
-  
+  int16_t shuntVoltageRaw;
+  if (monitor.shuntVoltageRaw(&shuntVoltageRaw)) {
+    Serial.println(shuntVoltageRaw);
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.print("raw bus voltage:   ");
-  Serial.println(monitor.busVoltageRaw());
-  
+  int16_t busVoltageRaw;
+  if (monitor.busVoltageRaw(&busVoltageRaw)) {
+    Serial.println(busVoltageRaw);
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.println("--");
-  
+
   Serial.print("shunt voltage: ");
-  Serial.print(monitor.shuntVoltage() * 1000, 4);
-  Serial.println(" mV");
-  
+  float shuntVoltage;
+  if (monitor.shuntVoltage(&shuntVoltage)) {
+    Serial.print(shuntVoltage * 1000, 4);
+    Serial.println(" mV");
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.print("shunt current: ");
-  Serial.print(monitor.shuntCurrent() * 1000, 4);
-  Serial.println(" mA");
-  
+  float shuntCurrent;
+  if (monitor.shuntCurrent(&shuntCurrent)) {
+    Serial.print(shuntCurrent * 1000, 4);
+    Serial.println(" mA");
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.print("bus voltage:   ");
-  Serial.print(monitor.busVoltage(), 4);
-  Serial.println(" V");
-  
+  float busVoltage;
+  if (monitor.busVoltage(&busVoltage)) {
+    Serial.print(busVoltage, 4);
+    Serial.println(" V");
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.print("bus power:     ");
-  Serial.print(monitor.busPower() * 1000, 4);
-  Serial.println(" mW");
-  
+  float busPower;
+  if (monitor.busPower(&busPower)) {
+    Serial.print(busPower * 1000, 4);
+    Serial.println(" mW");
+  } else {
+    Serial.println("failed");
+  }
+
   Serial.println(" ");
   Serial.println(" ");
-  
+
   delay(1000);
 }
 
